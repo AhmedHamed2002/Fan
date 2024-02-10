02e1c8dd84719a219d6fd308f19e885aa7df1676
Ahmed <ah1973826450@gmail.com>
2024-02-10 15:44:09 +0200

    project_9 --->  my fan

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let sp =  document.getElementById("m") ;
+
+    
+function fun_off(){
+    sp.style.animationDuration=  0 +"s";    
+}
+
+function fun_on(){
+    sp.style.animationDuration=  3 +"s";    
+}
+
+function fun_1(){
+    sp.style.animationDuration=  1 +"s";    
+}
+
+function fun_2(){
+    sp.style.animationDuration=  0.5 +"s";    
+}
+
+function fun_3(){
+    sp.style.animationDuration=  0.1 +"s";    
+}
